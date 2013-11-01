26dd9db5fe18f27bc56b0f4bc83c57300cc4fecf
Alex <alex.pana.oikonomou@gmail.com>
2013-11-01 17:29:02 +0200

    Initial files for the application

---
+++ src/com/alexoiko/alexalarmclock/DisplayMessageActivity.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.alexoiko.alexalarmclock;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * @author aoikonomou
+ *
+ */
+public class DisplayMessageActivity extends Activity {
+	
+	 public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+	@Override
+	@SuppressLint("NewApi")
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//this.setContentView(R.layout.activity_display_message);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+		Intent intent = this.getIntent();
+		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		
+		TextView textView = new TextView(this);
+		textView.setTextSize(40);
+		textView.setText(message);
+		
+		this.setContentView(textView);
+		
+
+	}
+	 @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+		 switch (item.getItemId()) {
+	        case android.R.id.home:
+	            NavUtils.navigateUpFromSameTask(this);
+	            return true;
+	        }
+	        return super.onOptionsItemSelected(item);
+		 
+	 }
+
+}
